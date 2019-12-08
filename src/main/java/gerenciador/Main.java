57ccf81f28034a7f09fe983acddcf298d4511687
Caroline Ribeiro <caroline.ribeiro@guiabolso.com.br>
2019-12-07 22:24:15 -0300

    :tada: primeiro commit

---
+++ src/main/java/gerenciador/Main.java
@@ -0,0 +1,19 @@
+package gerenciador;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns="/first")
+public class Main extends HttpServlet {
+    public Main() {
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
